b4b9bcbd3ffd57d3860ce09c6ae44ab5cd09f317
Efra <efragamer300@gmail.com>
2026-06-17 15:46:03 -0300

    auto: deploy formularacing-0.2
    Changes:
    M	src/main/java/dev/EfraGroup/formulaRacing/Utils/PlatformUtils.java
    
    Stats:
     1 file changed, 2 insertions(+), 16 deletions(-)

--- src/main/java/dev/EfraGroup/formulaRacing/Utils/PlatformUtils.java
+++ src/main/java/dev/EfraGroup/formulaRacing/Utils/PlatformUtils.java
@@ -1,10 +1,7 @@
 package dev.EfraGroup.formulaRacing.Utils;
 
-import org.bukkit.Bukkit;
-
 public final class PlatformUtils {
-    private static final boolean IS_FOLIA = isClassAvailable("io.papermc.paper.threadedregions.scheduler.GlobalRegionScheduler");
-    private static Boolean IS_FOLIA_RUNTIME = null;
+    private static final boolean IS_FOLIA = isClassAvailable("io.papermc.paper.threadedregions.RegionizedServer");
 
     private PlatformUtils() {}
 
@@ -13,18 +10,7 @@ public final class PlatformUtils {
     }
 
     public static boolean isFoliaRuntime() {
-        if (IS_FOLIA_RUNTIME == null) {
-            if (!IS_FOLIA) {
-                IS_FOLIA_RUNTIME = false;
-            } else {
-                try {
-                    IS_FOLIA_RUNTIME = !Bukkit.isPrimaryThread();
-                } catch (Exception e) {
-                    IS_FOLIA_RUNTIME = true;
-                }
-            }
-        }
-        return IS_FOLIA_RUNTIME;
+        return IS_FOLIA;
     }
 
     private static boolean isClassAvailable(String className) {
