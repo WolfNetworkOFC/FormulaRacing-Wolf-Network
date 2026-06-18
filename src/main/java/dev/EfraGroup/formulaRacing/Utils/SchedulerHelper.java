c6d5735e394b866748f9b788c7d4600d94be831a
Efra <efragamer300@gmail.com>
2026-06-18 20:19:54 -0300

    auto: deploy formularacing-0.2
    Changes:
    M	src/main/java/dev/EfraGroup/formulaRacing/Utils/SchedulerHelper.java
    
    Stats:
     1 file changed, 2 insertions(+), 1 deletion(-)

--- src/main/java/dev/EfraGroup/formulaRacing/Utils/SchedulerHelper.java
+++ src/main/java/dev/EfraGroup/formulaRacing/Utils/SchedulerHelper.java
@@ -99,7 +99,8 @@ public class SchedulerHelper {
 
     public static boolean teleport(Entity entity, Location destination) {
         if (PlatformUtils.isFolia()) {
-            return entity.teleportAsync(destination).join();
+            entity.teleportAsync(destination);
+            return true;
         } else {
             return entity.teleport(destination);
         }
